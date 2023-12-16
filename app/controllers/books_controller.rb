41da5fe22cff47777ab8407793f1d14276444afc
EC2 Default User <ec2-user@ip-172-31-19-70.ap-northeast-1.compute.internal>
2023-12-16 08:57:39 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+class BooksController < ApplicationController
+ def new
+    @book = Book.new
+ end
+
+ def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to books_path
+    else
+      flash[:notice] = "error"
+      @books = Book.all
+      render :index
+    end
+ end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "投稿削除に成功しました。"
+    redirect_to books_path
+  end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
