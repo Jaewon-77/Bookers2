f155970fd97cfef8eeff26379f6644535f2a33ba
EC2 Default User <ec2-user@ip-172-31-19-70.ap-northeast-1.compute.internal>
2024-01-04 10:39:43 +0000

    7a commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,12 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.all.sort {|a,b|
+      b.favorites.where(created_at: from...to).size <=>
+      a.favorites.where(created_at: from...to).size
+    }
     @book = Book.new
     @user = User.find(current_user.id)
     @book_comment = BookComment.new
